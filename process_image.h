2b1be98ad4c288ef4e88bf8a953397df5b968b27
Arwen <arwen.giraud@epfl.ch>
2020-05-01 17:11:56 +0200

    Comment pour capteur => problème de priorité?

--- process_image.h
+++ process_image.h
@@ -4,6 +4,6 @@
 uint8_t taille_obstacle(void);
 //void process_image(void);
 //void capture_image(void);
-void process_image_start(void);
+//void process_image_start(void);
 
 #endif /* PROCESS_IMAGE_H */
